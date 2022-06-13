976d5c602700555dccadb47331d0a764e9db1578
jinwapp <jinwaap@gmail.com>
2022-06-14 01:20:45 +0900

    // 문제: 문자열을 요소로 갖는 배열을 입력받아 문자열을 세로로 읽었을 때의 문자열을 리턴해야 합니다.

--- src/DailyCoding_readVertically.java
+++ src/DailyCoding_readVertically.java
@@ -1,31 +1,58 @@
+// 문제: 문자열을 요소로 갖는 배열을 입력받아 문자열을 세로로 읽었을 때의 문자열을 리턴해야 합니다.
+// 주의사항:
+// 각 문자열의 길이는 다양합니다.
+// 각 문자의 위치를 행, 열로 나타낼 경우, 비어있는 (행, 열)은 무시합니다.
+// 입출력 예시:
+//String[] input = new String[]{
+//        "hello",
+//        "wolrd",
+//        };
+//        String output = readVertically(input);
+//        System.out.println(output); // --> "hweolllrod"
+//
+//        input = new String[]{
+//        "hi",
+//        "wolrd",
+//        };
+//        output = readVertically(input);
+//        System.out.println(output); // --> "hwiolrd"
+
+import java.util.Arrays;
+
 public class DailyCoding_readVertically {
 
     public static void main(String[] args) {
 
-        String[] arr = {"hello","Word","Colaaa"};
+        String[] arr = {"aaa", "bbbb", "ccccc"};
 
-        //arr[0] = hello
-        //arr[1] = word
-        //arr[2] = colaaa
-        //arr[3] = abcdefg
+        int max = 0;
 
-        // for int i = 0 ~ arr.len -1까지 반복 수행
-            //arr[i].len > max
-                // max = arr[i].len
+        for (int i=0; i < arr.length; i++){
+            if(arr[i].length() > max){
+                max = arr[i].length();
+                System.out.println(max);
+            }
+        }
 
-        // for 0 ~ max-1
-            //for 0 ~ arr.length-1
-                // if arr[i] null이면
-                    //
-                // else
-                    //
+        String[] temp = new String[max];
 
-        // for int i = 0 ~ max 까지 돌린다
-            // if arr[i] != null이 아니면
-                // result += arr[i]
-            // if i <= arr
-                // result += b[i]출력
-        //return result ;
+        for (int i = 0; i < arr.length; i++){
+            String str = arr[i];
 
+            for (int j = 0 ; j < str.length() ; j++){
+                if (temp[j] == null){
+                    temp[j] = Character.toString(str.charAt(j));
+                    System.out.println(Arrays.toString(temp));
+                } else {
+                    temp[j] = temp[j] + str.charAt(j);
+                    System.out.println(Arrays.toString(temp));
+                }
+            }
+        }
+        String result = "";
+        for (int i = 0; i < temp.length; i++){
+            result += temp[i];
+            System.out.println(result);
+        }
     }
 }
