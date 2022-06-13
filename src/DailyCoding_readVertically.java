be46884babd89441564e2b69c9d000827b7eb036
jinwapp <jinwaap@gmail.com>
2022-06-13 23:02:13 +0900

    Initial commit

---
+++ src/DailyCoding_readVertically.java
@@ -0,0 +1,31 @@
+public class DailyCoding_readVertically {
+
+    public static void main(String[] args) {
+
+        String[] arr = {"hello","Word","Colaaa"};
+
+        //arr[0] = hello
+        //arr[1] = word
+        //arr[2] = colaaa
+        //arr[3] = abcdefg
+
+        // for int i = 0 ~ arr.len -1까지 반복 수행
+            //arr[i].len > max
+                // max = arr[i].len
+
+        // for 0 ~ max-1
+            //for 0 ~ arr.length-1
+                // if arr[i] null이면
+                    //
+                // else
+                    //
+
+        // for int i = 0 ~ max 까지 돌린다
+            // if arr[i] != null이 아니면
+                // result += arr[i]
+            // if i <= arr
+                // result += b[i]출력
+        //return result ;
+
+    }
+}
